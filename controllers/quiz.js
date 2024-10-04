50dca66b4203fa0cc14dee494f50ec926874786e
yennth2005 <ngohaiyen1237a6@gmail.com>
2024-10-04 07:36:06 +0700

    new

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,27 +1,77 @@
-import {getAllQuiz} from "../services/api.js";
+import {getAllQuiz, getQuestionByIdQuiz} from "../services/api.js";
 
 const app = {
-    // hiển thị ds các câu hỏi
+    // hiển thị ds các câu hỏi 
+    //async dùng để khai báo 1 hàm  bất đồng bộ
     renderListQuiz: async function(){
         //1. Lấy ds các câu hỏi
-        const data = await getAllQuiz(); // bất đồng bộ
-        console.log(data); // đồng bộ
+        const data = await getAllQuiz(); // bất đồng bộ //await chờ đợi xử lý bdb xong, gán kqua vào biến respone
+        // console.log(data); // đồng bộ
 
         //2. Đổ dữ liệu ra ds
         const listQuiz = data?.map((item,index)=>{
             // 2.1 Nếu trạng thái true thì mới hiển thị
             if(item.isActive){
                 return `
-                    <a href="#" class="quiz-items list-group-item list-group-item-action list-group-item-primary">
+                    <a href="#" data-id="${item.id}" class="quiz-items list-group-item list-group-item-action list-group-item-primary">
                         ${item.title} : ${item.description}
                     </a>
                 `
             }
         }).join("") // join chuyển 1 mảng về 1 chuỗi
-        console.log(listQuiz);
+        // console.log(listQuiz);
         // 2.2 lấy element (div #list_quiz )
         const listQuizElement = document.getElementById('list_quiz');
         //2.3 đổ ra element thông qua innerHTML
         listQuizElement.innerHTML = listQuiz;
+        this.handleClickQuiz();
     },
+    handleClickQuiz: function(){
+        //lấy ds mảng quiz
+        const quizItems = document.querySelectorAll(".quiz-items");
+        // console.log(quizItems);
+        
+        quizItems.forEach((item)=>{
+            //khai báo sự kiện
+            item.addEventListener('click',()=>{
+                //xác nhận hành động
+                //lấy tên tiêu đề từ nội dung
+                const title = item.textContent;
+                if(window.confirm(`Bạn có chắc chắn làm ${title}`)){
+                    //thêm thuộc tính
+                    const id = item.getAttribute("data-id");
+                    console.log(id);
+                    //chuyển trang câu hỏi
+                    window.location=`question.html?id=${id}`  ;
+                }
+            })
+            
+        })
+    },
+    renderListQuestion: async function(){
+        const data = await getAllQuiz(); // bất đồng bộ //await chờ đợi xử lý bdb xong, gán kqua vào biến respone
+        const listQuiz = data?.map((item,index)=>{
+            // 2.1 Nếu trạng thái true thì mới hiển thị
+            if(item.isActive){
+                return `
+                    <a href="#" data-id="${item.id}" class="quiz-items list-group-item list-group-item-action list-group-item-primary">
+                        ${item.title} : ${item.description}
+                    </a>
+                `
+            }
+        }).join("")
+        const quizItems = document.querySelectorAll(".quiz-items");
+        const questions= await getQuestionByIdQuiz(`${item.id}`);
+        console.log(questions);
+        
+        
+    },
+    start: function(){
+        // console.log("haien");
+        // render: Hiển thị giao diện
+        // handle: Thực thi logic
+        this.renderListQuiz();
+        this.renderListQuestion();
+    }
 }
+app.start()
--- services/api.js
+++ services/api.js
@@ -9,3 +9,15 @@ export const getAllQuiz = async ()=>{
         alert("error")
     }
 }
+
+export const getQuestionByIdQuiz = async(idQuiz)=>{
+    try {
+        // call api lấy ds question(câu hỏi theo id của quiz)
+        const res = await fetch(`http://localhost:3000/questions?quizId=${idQuiz}`); // call api:bất đồng bộ
+        const data = await res.json()
+        // console.log(data); // đồng bộ
+        return data;
+    } catch (error) {
+        alert("error")
+    }
+}
